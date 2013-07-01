10a36c7a64826d0b7c9c5e2c1d936e8c046453da
Ian O Connell <ian@knobout.com>
2013-06-30 22:55:32 -0700

    Make GPIO to led color mapping configurable in conf file

--- src/main/scala/com/example/Driver.scala
+++ src/main/scala/com/example/Driver.scala
@@ -9,11 +9,22 @@ import com.typesafe.config._
 
 object Driver {
   val gpio = GpioFactory.getInstance();
-  val red_led = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04)
-  val blue_led = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06)
-  val green_led = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01)
-
+  val cfg_map = List(RaspiPin.GPIO_00, RaspiPin.GPIO_01, RaspiPin.GPIO_02,
+    RaspiPin.GPIO_03, RaspiPin.GPIO_04, RaspiPin.GPIO_05, RaspiPin.GPIO_06,
+    RaspiPin.GPIO_07, RaspiPin.GPIO_08, RaspiPin.GPIO_09, RaspiPin.GPIO_10,
+    RaspiPin.GPIO_11, RaspiPin.GPIO_12, RaspiPin.GPIO_13, RaspiPin.GPIO_14,
+    RaspiPin.GPIO_15, RaspiPin.GPIO_16, RaspiPin.GPIO_17, RaspiPin.GPIO_18,
+    RaspiPin.GPIO_19, RaspiPin.GPIO_20).map{case x => (x.getName(), x)}.toMap
+    
   val conf = ConfigFactory.load();
+  def loadPin(pin_name:String): GpioPinDigitalOutput = {
+    val led_enum = cfg_map.getOrElse(conf.getString(pin_name), null)
+    if(led_enum == null) throw new Exception("Not found led") else gpio.provisionDigitalOutputPin(led_enum)
+  }
+  
+  val red_led = loadPin("red_led")
+  val blue_led = loadPin("blue_led")
+  val green_led = loadPin("green_led")
 
   val username = conf.getString("username")
   val password = conf.getString("password")
